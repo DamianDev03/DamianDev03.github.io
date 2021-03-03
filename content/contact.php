bca29098cda6682691e938fe7ef318ccc6b0676f
yoran <yorandeblaey@gmail.com>
2021-03-03 12:32:28 +0100

    sociaalmedia link

--- content/contact.php
+++ content/contact.php
@@ -154,10 +154,10 @@ if($_POST) {
      
   <div class="social-footer-icons">
     <ul class="menu simple">
-      <li><a href="https://www.facebook.com/"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
-      <li><a href="https://www.instagram.com/?hl=en"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
-      <li><a href="https://www.pinterest.com/"><i class="fa fa-pinterest-p" aria-hidden="true"></i></a></li>
-      <li><a href="https://twitter.com/?lang=en"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+      <li><a href="https://www.facebook.com/"><i>Facebook</i></a></li>
+      <li><a href="https://www.instagram.com/?hl=en"><i>Instagram</i></a></li>
+      <li><a href="https://www.pinterest.com/"><i>Pintrest</i></a></li>
+      <li><a href="https://twitter.com/?lang=en"><i>Twitter</i></a></li>
     </ul>
   </div>
     </div>
