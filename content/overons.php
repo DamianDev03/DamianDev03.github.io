04caf21b183081c6bb2b319530a28ae7f03d21c7
yoran <yorandeblaey@gmail.com>
2021-03-04 09:07:09 +0100

    footer goed

--- content/overons.php
+++ content/overons.php
@@ -33,5 +33,41 @@
 
 	</div>
 	
+	<div class="footer">
+<div class="container">
+  <div class="row">
+    <div class="col-lg-5 col-xs-12 about-company">
+     
+  <div class="social-footer-icons">
+    <ul class="menu simple">
+      <li><a href="https://www.facebook.com/"><i>Facebook</i></a></li>
+      <li><a href="https://www.instagram.com/?hl=en"><i>Instagram</i></a></li>
+      <li><a href="https://www.pinterest.com/"><i>Pintrest</i></a></li>
+      <li><a href="https://twitter.com/?lang=en"><i>Twitter</i></a></li>
+    </ul>
+  </div>
+    </div>
+    <div class="col-lg-3 col-xs-12 links">
+      <h4 class="mt-lg-0 mt-sm-3">Links</h4>
+        <ul class="m-0 p-0">
+          <li>- <a href="../index.php">Home</a></li>
+          <li>- <a href="overons.php">Over ons</a></li>
+          <li>- <a href="abonnementen.php">Abonnementen</a></li>
+          <li>- <a href="contact.php">Contact</a></li>
+        </ul>
+    </div>
+    <div class="col-lg-4 col-xs-12 location">
+      <h4 class="mt-lg-0 mt-sm-4">Location</h4>
+      <p>Meester F.J. Haarmanweg 56-32, 4538 AS Terneuzen</p>
+      <p class="mb-0"><i class="fa fa-phone"></i>06-32538464</p>
+      <p><i class="fa fa-envelope"></i>info-tele4@outlook.com</p>
+    </div>
+  </div>
+  <div class="row mt-5">
+    <div class="col copyright">
+      <p class=""><small class="text-white-50">© 2021. Damian van der Sluis, Yoran de Blaey.</small></p>
+    </div>
+  </div>
+</div>
 </body>
 </html>
